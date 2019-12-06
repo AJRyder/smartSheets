b117278658c0439d04f0773c64c91e39a6d0de76
Aaron Ryder <aaronryder@Aarons-MacBook-Pro.local>
2019-12-05 16:59:11 -0800

    adds app.js render and class App functionality for addprofilepic componentdidmount and dosetcurrentuser

--- models/Users.js
+++ models/Users.js
@@ -3,8 +3,8 @@ const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
     displayName: String, 
-    username: { String, unique=true }, 
-    email: { String, unique=true },
+    username: String, 
+    email: String,
     password: String, 
     avatar: String, 
     productionsList: [{
--- src/App.js
+++ src/App.js
@@ -2,14 +2,53 @@ import React, { Component } from 'react';
 import { Route, Switch } from 'react-router-dom'
 
 import NavBar from './components/NavBar'
+import SignInWithGoogle from './components/SignInWithGoogle'
+import SignUpWithEmailPassWord from './components/SignUpWithEmailPassword'
 
 import * as ROUTES from './constants/routes'
+import { firebase, doAddFile, auth, doSignOut, doSignInWithEmailAndPassWord } from './firebase/firebase'
+
 import './App.css';
 
 class App extends Component {
   state = { 
     message: '', 
-    currentUser: null 
+    currentUser: null, 
+    file: null  
+  }
+  addProfilePicture = event => {
+    doAddFile(event.target.files[0])
+      .then(file => file.ref.getDownloadURL())
+      .then(async url => {
+        const updatedUser = await fetch(`auth/users/${this.state.currentUser._id}`, {
+        method: 'PUT', 
+        body: JSON.stringify({ imgUrl: url }), 
+        headers: { 
+          'Content-Type': 'application/json'
+        }
+      })
+      const updatedUserJSon = await updatedUser.json()
+      this.doSetCurrentUser(updatedUserJSon)
+    })
+  }
+
+  async componentDidMount() { 
+    const message= await fetch('/hello')
+    const messageJson = await message.json()
+    this.setState({message: messageJson.message})
+    auth.onAuthStateChanged(authUser => { 
+      authUser
+      ? this.setState({currentUser: {
+        displayName: authUser.email}
+      })
+      : this.setState({ currentUser: null })
+    })     
+  }
+
+  doSetCurrentUser = currentUser => { 
+    this.setState({
+      currentUser
+    })
   }
   render() {
     const { currentUser } = this.state
@@ -19,16 +58,25 @@ class App extends Component {
         <NavBar />
         {
           currentUser
-          ? currentUser.displayName
+          ? <div> 
+              {currentUser.displayName}
+              <button onClick={doSignOut}>Sign Out</button>
+              <img src={currentUser.imgUrl} />
+            </div>
           : null
         }
-        <h1>Hey Dude!</h1>
+        <h1>Hey I'm the app page</h1>
+        <input type='file' onChange={this.addProfilePicture} accept='image/*'/>
+        <SignInWithGoogle doSetCurrentUser={this.doSetCurrentUser}/>
         <Switch>
-          
+          <Route exact path={ROUTES.HOME} render={() => <div>home</div>}/>
+          {/* <Route exact path={ROUTES.LOGIN} render={() => <div>login</div>}/> */}
+          <Route exact path={ROUTES.SIGN_UP} component={SignUpWithEmailPassWord}/>
+         
         </Switch>  
       </div>
     );
-}
+  }
 }
 
 export default App;
